c7d1600c799ab861eabaee04b09b07af53e3c97e
jirkapriv <jiri.privratsky@student.spsmb.cz>
2023-02-08 10:50:03 +0100

    new commit

--- res/js/script.js
+++ res/js/script.js
@@ -1,21 +1,23 @@
 const bar = document.getElementById("bar");
 const barsIcon = document.getElementById("barsIcon");
 const crossIcon = document.getElementById("crossIcon");
+const vpravoIcon = document.getElementById("vpravoIcon");
+const podMenu = document.getElementById("podMenu");
 
 barsIcon.onclick = () => {
-  bar.style.height = "500px";
+  bar.style.height = "250px";
   barsIcon.style.display = "none";
   crossIcon.style.display = "unset";
-
-  
-
-
-
-
+  vpravoIcon.style.display = "none";
+  podMenu.style.display = "flex";
+  podMenu.style.justifyContent = "center";
+  podMenu.style.alignItems = "center";
 };
 
 crossIcon.onclick = () => {
   bar.style.height = "50px";
   barsIcon.style.display = "unset";
   crossIcon.style.display = "none";
+  vpravoIcon.style.display = "flex";
+  podMenu.style.display = "none";
 };
