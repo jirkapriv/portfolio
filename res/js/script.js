b6a74c9798ec340161483dd60b67bc301a9b09b0
jirkapriv <jiri.privratsky@student.spsmb.cz>
2023-08-30 13:20:01 +0200

    bugs

--- res/js/script.js
+++ res/js/script.js
@@ -5,19 +5,40 @@ const vpravoIcon = document.getElementById("vpravoIcon");
 const podMenu = document.getElementById("podMenu");
 
 barsIcon.onclick = () => {
+  podMenu.style.transition = "1s linear";
   bar.style.height = "300px";
   barsIcon.style.display = "none";
   crossIcon.style.display = "unset";
-  vpravoIcon.style.display = "none";
-  podMenu.style.opacity = 1;
-  podMenu.style.transition = "1.5s linear";
+  vpravoIcon.style.display = "none"
+  podMenu.style.opacity = 0;
+  setTimeout(() => {
+    podMenu.style.display = "flex";
+    setTimeout(() => {
+      podMenu.style.opacity = 1;
+    }, "100");
+  }, "100");
 };
 
 crossIcon.onclick = () => {
+
+
+
   bar.style.height = "50px";
   barsIcon.style.display = "unset";
   crossIcon.style.display = "none";
-  vpravoIcon.style.display = "flex";
-  podMenu.style.opacity = 0;
+  podMenu.style.display = "none";
   podMenu.style.transition = "0s";
 };
+
+onresize = () =>{
+
+    if (window.innerWidth > 1250 + "px" && podMenu.style.display == "none"){
+
+    vpravoIcon.style.display = "flex"
+  
+  
+  }
+
+
+}
+
