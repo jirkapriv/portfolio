772065faba32355e6ea661096a861a8cb77c4d74
jirkapriv <jiri.privratsky@student.spsmb.cz>
2023-08-30 13:35:52 +0200

    fixed bug lack of contact page content

--- res/js/script.js
+++ res/js/script.js
@@ -9,7 +9,7 @@ barsIcon.onclick = () => {
   bar.style.height = "300px";
   barsIcon.style.display = "none";
   crossIcon.style.display = "unset";
-  vpravoIcon.style.display = "none"
+  vpravoIcon.style.display = "none";
   podMenu.style.opacity = 0;
   setTimeout(() => {
     podMenu.style.display = "flex";
@@ -20,9 +20,6 @@ barsIcon.onclick = () => {
 };
 
 crossIcon.onclick = () => {
-
-
-
   bar.style.height = "50px";
   barsIcon.style.display = "unset";
   crossIcon.style.display = "none";
@@ -30,15 +27,11 @@ crossIcon.onclick = () => {
   podMenu.style.transition = "0s";
 };
 
-onresize = () =>{
-
-    if (window.innerWidth > 1250 + "px" && podMenu.style.display == "none"){
-
-    vpravoIcon.style.display = "flex"
-  
-  
+onresize = () => {
+  if (window.innerWidth > 1250 && podMenu.style.display === "none") {
+    vpravoIcon.style.display = "flex";
   }
-
-
-}
-
+  if (window.innerWidth < 1200) {
+    vpravoIcon.style.display = "none";
+  }
+};
